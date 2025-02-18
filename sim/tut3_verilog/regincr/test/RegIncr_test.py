8f8fb5585eff44c55dd16790d6aff97720d32dd0
cb535 <cb535@cornell.edu>
2025-02-18 10:38:17 -0500

    staff update from: ece6745-sec01-asic-front-end-0.0-1384-g1f489e0 [ci skip]

--- sim/tut3_verilog/regincr/test/RegIncr_test.py
+++ sim/tut3_verilog/regincr/test/RegIncr_test.py
@@ -36,5 +36,34 @@ def test_large( cmdline_opts ):
 # This test script is incomplete. As part of the tutorial you will add
 # another test case to test for overflow. Later you will add a test case
 # for random testing.
-# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''\/
 
+#-------------------------------------------------------------------------
+# test_overflow
+#-------------------------------------------------------------------------
+
+def test_overflow( cmdline_opts ):
+  run_test_vector_sim( RegIncr(), [
+    ('in_   out*'),
+    [ 0x00, '?'  ],
+    [ 0xfe, 0x01 ],
+    [ 0xff, 0xff ],
+    [ 0x00, 0x00 ],
+  ], cmdline_opts )
+
+#-------------------------------------------------------------------------
+# test_random
+#-------------------------------------------------------------------------
+
+import random
+
+def test_random( cmdline_opts ):
+
+  test_vector_table = [( 'in_', 'out*' )]
+  last_result = '?'
+  for i in range(20):
+    rand_value = Bits8( random.randint(0,0xff) )
+    test_vector_table.append( [ rand_value, last_result ] )
+    last_result = Bits8( rand_value + 1, trunc_int=True )
+
+  run_test_vector_sim( RegIncr(), test_vector_table, cmdline_opts )
